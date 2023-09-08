d8916c87288133a3cec9b02baffb6890621fa5fa
Hoang <hoangnh190519962@gmail.com>
2023-09-08 20:01:04 +0700

    update

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,5 +1,3 @@
-
-
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 import React, { useState } from "react";
@@ -22,12 +20,11 @@ import {
   NavbarText,
   Button,
 } from "reactstrap";
-import Badge from '@mui/material/Badge';
+import Badge from "@mui/material/Badge";
 import { useTranslation } from "react-i18next";
 import i18n from "../i18n/i18n";
 
-
-export function Header({transparent}) {
+export function Header({ transparent }) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
@@ -37,71 +34,76 @@ export function Header({transparent}) {
 
   const { t } = useTranslation("translation");
   const onClickVN = () => {
-   
     i18n.changeLanguage("vie");
-  }
+  };
   const onClickEN = () => {
-   
     i18n.changeLanguage("eng");
-  }
+  };
 
   return (
-    <div>
-      <Navbar  expand className="navbar" style={{backgroundColor: `${transparent}`}}>
-        <NavbarBrand href="/" className="navbarBrand">
-          <i class="fa-solid fa-seedling" ></i>
-        </NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="me-auto" navbar>
-        
-
-            <NavLink>
-              <Link to="/bmi" className="navbarBrand">
-                BMI &nbsp;
-              </Link>
-            </NavLink>
-
-            <NavLink>
-              <Link to="/calculatorcalo" className="navbarBrand">
-               {t("calorie_calculator")} &nbsp;
-              </Link>
-            </NavLink>
-
-            <NavLink >
-              <Link to="/dailycalo" className="navbarBrand">
-              {t("daily_calorie")}
-              </Link>
-            </NavLink>
-
-            <UncontrolledDropdown nav inNavbar>
-              <DropdownToggle nav caret>
+    <>
+      <div>
+        <nav
+          class="navbar navbar-expand-lg navbar-light b"
+          style={{ backgroundColor: `${transparent}` }}
+        >
+          <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+              {" "}
+              <i class="fa-solid fa-seedling"></i>
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarSupportedContent"
+              aria-controls="navbarSupportedContent"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div
+              class="collapse navbar-collapse d-flex justify-content-between"
+              id="navbarSupportedContent"
+            >
+              <div>
+                <Link to="/bmi" className="navbarBrand" style={{textDecoration: "none"}}>
+                  BMI &nbsp;&nbsp;&nbsp;
+                </Link>
+                <Link to="/calculatorcalo" className="navbarBrand">
+                  {t("calorie_calculator")} &nbsp;&nbsp;&nbsp;
+                </Link>
+                <Link to="/dailycalo" className="navbarBrand">
+                  {t("daily_calorie")}&nbsp;&nbsp;&nbsp;
+                </Link>
                 <Link className="navbarBrand"> {t("gallery")}</Link>
-              </DropdownToggle>
-              <DropdownMenu right className="navbarBrand">
-                <DropdownItem className="navbarBrand">Food</DropdownItem>
-                <DropdownItem className="navbarBrand">Activities</DropdownItem>
-              </DropdownMenu>
-            </UncontrolledDropdown>
-          </Nav>
-
-          <div onClick={() => navigate("/cart-detail")}  style={{cursor: "pointer"}}>
-          
-
-
-            <Badge badgeContent={cartItem.length}  color="error"  size="large">
-            <ShoppingCartIcon />
-            </Badge>
+              </div>
+              <div>
+                <div
+                  onClick={() => navigate("/cart-detail")}
+                  style={{ cursor: "pointer" }}
+                >
+                  <Badge
+                    badgeContent={cartItem.length}
+                    color="error"
+                    size="large"
+                  >
+                    <ShoppingCartIcon />
+                  </Badge>
+                  <Link to="/admin" className="navbarBrand">
+                    {t("title_admin")}
+                  </Link>
+                  <img src={EN} width={"30px"} onClick={() => onClickEN()} />{" "}
+                  &nbsp;
+                  <img src={VN} width={"30px"} onClick={() => onClickVN()} />
+                </div>
+              </div>
+            </div>
           </div>
-          <Link to="/admin" className="navbarBrand">
-          {t("title-admin")}
-          </Link>
-        
-         <img src={EN} width={"30px"} onClick={() => onClickEN()}/> &nbsp;
-         <img src={VN} width={"30px"} onClick={() => onClickVN()}/>
-        </Collapse>
-      </Navbar>
-    </div>
+        </nav>
+      </div>
+    </>
   );
 }
 
